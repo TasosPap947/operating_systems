8de111434fe742d42087e413ca85ac3480d99c82
nikpag00 <nikospagonas00@gmail.com>
2021-03-20 23:55:44 +0200

    Use getlogin() instead of cuserid() in zing2

--- 1/1.1/zing2.c
+++ 1/1.1/zing2.c
@@ -1,9 +1,7 @@
-#define _XOPEN_SOURCE
 #include <stdio.h>
 #include <unistd.h>
-char username[L_cuserid];
 
 void zing(void) {
-	cuserid(username);
-	printf("Goodbye, %s\n", username);
+	char *username = getlogin();
+	printf("%s sucks at coding.\n", username);
 }
