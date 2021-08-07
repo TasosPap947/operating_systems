684d74ddeb1593994e91e51b3041608873671e8e
nikpag00 <nikospagonas00@gmail.com>
2021-08-07 03:06:25 +0300

    Cleanup files

--- 1/1.1/zing2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-void zing(void) {
-	char *username = getlogin();
-	printf("%s sucks at coding.\n", username);
-}
--- 3/code/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int x = 2;
-  int *p = x;
-  *p = *p + 1;
-  printf("%d\n",&p);
-  printf("%d\n",p);
-}
